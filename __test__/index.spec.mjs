b8c3a49ccad4b096310cc166aa574db0df23197a
xHyroM <generalkubo@gmail.com>
2022-07-09 12:15:16 +0200

    0.1.0

--- __test__/index.spec.mjs
+++ __test__/index.spec.mjs
@@ -3,7 +3,7 @@ import test from 'ava'
 import { runCommand } from '../index.js'
 
 test('run_command from native', (t) => {
-  t.is(true, true);
+  t.is(true, true); // remove
   // TODO: update because windows
   //t.is(runCommand('echo', process.cwd(), ['test']), 'test\n');
 })
